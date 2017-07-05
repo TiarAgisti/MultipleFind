6f3a9826b48d88cf303282e06a1dccc2fc6849f7
TGN01-PBTGROUP\tiara021600167 <tiar.agisti@gmail.com>
2017-07-05 10:49:28 +0700

    create repository
    create new repository

---
+++ Form1.Designer.vb
@@ -0,0 +1,180 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtName = New System.Windows.Forms.TextBox()
+        Me.txtAddress = New System.Windows.Forms.TextBox()
+        Me.txtEmail = New System.Windows.Forms.TextBox()
+        Me.txtPhone = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.btnFind = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.dgv = New System.Windows.Forms.DataGridView()
+        Me.Panel1.SuspendLayout()
+        CType(Me.dgv, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'txtName
+        '
+        Me.txtName.Location = New System.Drawing.Point(81, 11)
+        Me.txtName.Name = "txtName"
+        Me.txtName.Size = New System.Drawing.Size(181, 20)
+        Me.txtName.TabIndex = 0
+        '
+        'txtAddress
+        '
+        Me.txtAddress.Location = New System.Drawing.Point(81, 37)
+        Me.txtAddress.Name = "txtAddress"
+        Me.txtAddress.Size = New System.Drawing.Size(181, 20)
+        Me.txtAddress.TabIndex = 1
+        '
+        'txtEmail
+        '
+        Me.txtEmail.Location = New System.Drawing.Point(81, 89)
+        Me.txtEmail.Name = "txtEmail"
+        Me.txtEmail.Size = New System.Drawing.Size(181, 20)
+        Me.txtEmail.TabIndex = 3
+        '
+        'txtPhone
+        '
+        Me.txtPhone.Location = New System.Drawing.Point(81, 63)
+        Me.txtPhone.Name = "txtPhone"
+        Me.txtPhone.Size = New System.Drawing.Size(181, 20)
+        Me.txtPhone.TabIndex = 2
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(7, 14)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(35, 13)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "Name"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(7, 40)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(45, 13)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "Address"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(7, 92)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(32, 13)
+        Me.Label3.TabIndex = 7
+        Me.Label3.Text = "Email"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(7, 66)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(38, 13)
+        Me.Label4.TabIndex = 6
+        Me.Label4.Text = "Phone"
+        '
+        'btnFind
+        '
+        Me.btnFind.Location = New System.Drawing.Point(81, 115)
+        Me.btnFind.Name = "btnFind"
+        Me.btnFind.Size = New System.Drawing.Size(75, 23)
+        Me.btnFind.TabIndex = 8
+        Me.btnFind.Text = "Find"
+        Me.btnFind.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(162, 115)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 23)
+        Me.btnExit.TabIndex = 9
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'Panel1
+        '
+        Me.Panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Panel1.Controls.Add(Me.txtName)
+        Me.Panel1.Controls.Add(Me.btnExit)
+        Me.Panel1.Controls.Add(Me.txtAddress)
+        Me.Panel1.Controls.Add(Me.btnFind)
+        Me.Panel1.Controls.Add(Me.txtPhone)
+        Me.Panel1.Controls.Add(Me.Label3)
+        Me.Panel1.Controls.Add(Me.txtEmail)
+        Me.Panel1.Controls.Add(Me.Label4)
+        Me.Panel1.Controls.Add(Me.Label1)
+        Me.Panel1.Controls.Add(Me.Label2)
+        Me.Panel1.Dock = System.Windows.Forms.DockStyle.Top
+        Me.Panel1.Location = New System.Drawing.Point(0, 0)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(717, 148)
+        Me.Panel1.TabIndex = 10
+        '
+        'dgv
+        '
+        Me.dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgv.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.dgv.Location = New System.Drawing.Point(0, 148)
+        Me.dgv.Name = "dgv"
+        Me.dgv.Size = New System.Drawing.Size(717, 274)
+        Me.dgv.TabIndex = 11
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(717, 422)
+        Me.Controls.Add(Me.dgv)
+        Me.Controls.Add(Me.Panel1)
+        Me.Name = "Form1"
+        Me.Text = "Customers"
+        Me.WindowState = System.Windows.Forms.FormWindowState.Maximized
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        CType(Me.dgv, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents txtName As TextBox
+    Friend WithEvents txtAddress As TextBox
+    Friend WithEvents txtEmail As TextBox
+    Friend WithEvents txtPhone As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents btnFind As Button
+    Friend WithEvents btnExit As Button
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents dgv As DataGridView
+End Class
---
+++ Form1.vb
@@ -0,0 +1,52 @@
+﻿Imports MySql.Data
+Imports MySql.Data.MySqlClient
+Public Class Form1
+
+    Protected myConnectionString As String = "Server=Localhost;Database=vbnet;User Id=root;Password=tiar123"
+    Function FindData() As DataTable
+        Dim sql As String = "Select * From Customers Where IsActive = 1"
+
+        If Not String.IsNullOrEmpty(txtName.Text) Then
+            sql += " AND CustomerName LIKE '%" & txtName.Text & "%'"
+        End If
+
+        If Not String.IsNullOrEmpty(txtAddress.Text) Then
+            sql += " AND Address LIKE '%" & txtAddress.Text & "%'"
+        End If
+
+        If Not String.IsNullOrEmpty(txtPhone.Text) Then
+            sql += " AND Phone LIKE '%" & txtPhone.Text & "%'"
+        End If
+
+        If Not String.IsNullOrEmpty(txtEmail.Text) Then
+            sql += " AND Email LIKE '%" & txtEmail.Text & "%'"
+        End If
+
+        Dim dataTable As DataTable = New DataTable
+        Try
+            Using conn = New MySqlConnection(myConnectionString)
+                conn.Open()
+                Dim myCommand As MySqlCommand = New MySqlCommand(sql, conn)
+                Using dataAdapter As MySqlDataAdapter = New MySqlDataAdapter(myCommand)
+                    dataAdapter.Fill(dataTable)
+                End Using
+                myCommand.Dispose()
+                conn.Close()
+                Return dataTable
+            End Using
+        Catch ex As Exception
+            Throw ex
+        End Try
+    End Function
+    Private Sub btnFind_Click(sender As Object, e As EventArgs) Handles btnFind.Click
+        dgv.DataSource = FindData()
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Close()
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        dgv.DataSource = FindData()
+    End Sub
+End Class
